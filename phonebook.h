de8bf6ed6810cbab233ec6c28e1a4e073c4067b0
Christopher Cox <ccox04@vt.edu>
2013-12-11 11:00:16 -0500

    changed some functions in the header

--- phonebook.h
+++ phonebook.h
@@ -21,22 +21,32 @@ public:
   PhoneRecord(istream &input);
   //returns first name
   string first_name() const;
+
   //returns last name
   string last_name() const;
+
   //return phone number
   string phone_num() const;
+
   //finds the correct contact and returns the info
   string search();
-  //prints the contact record
+
+  //checks for correct formatting
+  string Fsearch();
+
+  //prints the contact based on user input.(-s,-a)
   void print_record();
-  //loads the contact record from input file or cin
+
+  //loads the contact from search if it is in correct format
   void load_record();
 
 private:
-  //first name variable
+  	//first name variable
 	string Fname;
+
 	//last name variable
 	string Lname;
+
 	//phone number variable
 	string Pnumber;
 };
