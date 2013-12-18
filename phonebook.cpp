496ee4efeba234292badc063c1429f89af21afd4
Christopher Cox <ccox04@vt.edu>
2013-12-18 14:05:54 -0500

    fixed the search function

--- phonebook.cpp
+++ phonebook.cpp
@@ -55,13 +55,12 @@ void PhoneRecord::viewContacts(){
 string PhoneRecord::search(string contacts)
 {
   int index;
-  string In_line;
   ifstream searchfile;
   searchfile.open("phonebook.txt");
   while(!searchfile.eof())
     {
-      getline(searchfile,In_line);
-      if((index = In_line.find(seachfile,0)) != string::npos)
+      getline(searchfile,contacts);
+      if((index = contacts.find(seachfile,0)) != string::npos)
 	{
 	  cout << "The contact(s) you searched for are: \n" << endl;
 	  cout << searchfile << endl;
